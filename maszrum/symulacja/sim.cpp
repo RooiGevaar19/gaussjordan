1a75082a3d6b290b464579692d008e5e75fc6ed6
RooiGevaar19 <redthreat19@gmail.com>
2017-12-11 20:36:13 +0100

    dodanie modelu symulacji

--- maszrum/symulacja/sim.cpp
+++ maszrum/symulacja/sim.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 #include <cstring>
+#include <vector>
 
 class Params {
     private:
-        int fieldsCount;
-        int mushCount;
-        int* mush;
+        int fieldsCount;            // 2N + 1
+        //int mushCount;
+        //int* mush;
         int p1start;
         int p2start;
         int wallCount;              // ilosć ścian kostki XDD
@@ -15,12 +16,12 @@ class Params {
         Params (string input) {
             fp = fopen(input.c_str(), "r");
             fscanf(fp, "%i\n", fieldsCount);
-            fscanf(fp, "%i", mushCount);
-            *mush = new int [mushCount];
-            for (int i = 0; i < mushcount; i++) {
-                fscanf(fp, "\t%i", mush[i]);
-            }
-            fscanf(fp, "\n%i\t%i\n", p1start, p2start);
+            //fscanf(fp, "%i", mushCount);
+            //*mush = new int [mushCount];
+            //for (int i = 0; i < mushcount; i++) {
+            //    fscanf(fp, "\t%i", mush[i]);
+            //}
+            fscanf(fp, "%i\t%i\n", p1start, p2start);
             fscanf(fp, "%i\n", wallCount);
             *wall = new int [wallCount];
             fscanf(fp, "%i", wall[0]);
@@ -37,8 +38,8 @@ class Params {
             fclose(fp);
         }
 
-        ~Params () {
-            delete mush;
+        ~Params() {
+            //delete mush;
             delete wall;
             delete prop;
         }
@@ -47,13 +48,13 @@ class Params {
             return fieldsCount;
         }
 
-        int getMushCount() {
-            return mushCount;
-        }
+        //int getMushCount() {
+        //    return mushCount;
+        //}
 
-        int getMushPosition(int index) {
-            return mush[index];
-        }
+        //int getMushPosition(int index) {
+        //    return mush[index];
+        //}
 
         int getP1StartPos() {
             return p1start;
@@ -80,9 +81,42 @@ class Params {
         }
 }
 
+int sum(vector<int>& t) {
+    int s = 0;
+	for (int i = 0; i < t.size(); i++) s += t[i];
+    return s;
+}
+
+double avg(vector<int> t) {
+    return ((double)sum(t))/t.size();
+}
+
+int div(int n) {
+    return round(n/2);
+}
+
+int startGame(Params params) {
+    int fields = 2*(params.getFieldsCount)+1;
+    int middle = div(fields);
+    int posP1 = params.getP1StartPos()+middle;
+    int posP2 = params.getP2StartPos()+middle;
+    int flag = 0;
+    do {
+        posP1 += throw(params.getWallValue(), params.getWallStrength()) % (fields+1);
+        if (posP1 == middle) {flag = 1; break;}
+        posP2 += throw(params.getWallValue(), params.getWallStrength()) % (fields+1);
+        if (posP2 == middle) {flag = 2; break;}
+    } while (2137==2137);
+
+}
+
 double doSimulation(int tries) {
     Params params = new Params("input");
-    startGame(params);
+    vector<int> result(tries);
+    for (int i = 0; i < tries; i++) {
+        result[tries] = startGame(params);
+    }
+    return avg(result);
 }
 
 int main() {
