bd025ea89408de429613a748edff21f6978a4af7
RooiGevaar19 <redthreat19@gmail.com>
2017-12-14 21:32:14 +0100

    gauss gra ze mnom w pokera XD

--- maszrum/generowanie/GenerowanieJava/MatrixGenerator.java
+++ maszrum/generowanie/GenerowanieJava/MatrixGenerator.java
@@ -27,26 +27,30 @@ public class MatrixGenerator {
      int fields = 2*(par.getFieldsCount())+1;
      xs.add(new Entry(i+1, 0, par.getP1StartPos(), par.getP2StartPos()));
      boolean anythingleft;
-     do {
-         anythingleft = false;
-    	 for (int k : dic.GetFields()) {
-             if (!exists(xs, 
+     try {
+    	 do {
+    		 anythingleft = false;
+    		 for (int k : dic.GetFields()) {
+    			 if (!exists(xs, 
             		 		((xs.get(step).getPlayer()+1)%2), 
             		 		Math.floorMod(((xs.get(step).getPosP1())+(k*((xs.get(step).getPlayer()+1)%2))+3), fields)-3, 
             		 		Math.floorMod(((xs.get(step).getPosP2())+(k*((xs.get(step).getPlayer())%2))+3), fields)-3
             		 		)) {
-            	 anythingleft = true;
-                 xs.add(new Entry(
+    				 anythingleft = true;
+    				 xs.add(new Entry(
                 		 i+2, 
                 		 ((xs.get(step).getPlayer()+1)%2), 
                 		 Math.floorMod(((xs.get(step).getPosP1())+(k*((xs.get(step).getPlayer()+1)%2))+3), fields)-3, 
                 		 Math.floorMod(((xs.get(step).getPosP2())+(k*((xs.get(step).getPlayer())%2))+3), fields)-3
                 		 ));
-                 i++;
-             }
-         }
-         step++;
-     } while (anythingleft);
+    				 i++;
+    			 }
+    		 }
+    		 step++;
+    	 } while (2137==2137);	// (i <= 96) dzia³a bez wyj¹tku, no i ofc (anythingleft)
+     } catch (Exception e) {
+    	 //System.out.println(i+1);
+     }
 
      for (Entry v : xs) {
     	 System.out.println("Var "+(v.getID())+" - P"+(v.getPlayer()+1)
