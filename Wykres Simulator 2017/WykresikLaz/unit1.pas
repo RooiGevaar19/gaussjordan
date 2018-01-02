5a3c4a489dec801a3c377f05296dfea7da271196
RooiGevaar19 <redthreat19@gmail.com>
2018-01-02 14:09:49 +0100

    pierwsze podejscie do czwartego projektu

---
+++ Wykres Simulator 2017/WykresikLaz/unit1.pas
@@ -0,0 +1,30 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, TAGraph, Forms, Controls, Graphics, Dialogs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Chart1: TChart;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
